0d8c31be23e7b98c174a3204d8c9fee17436114b
samer shili <you@example.com>
2025-12-12 15:59:42 +0100

    samer is yetrena

--- utils/file_loader.py
+++ utils/file_loader.py
@@ -1,20 +1,19 @@
 import glob
 import os
+
 import pandas as pd
-from utils.constants import DATASET_FLAG, DATA_PATH, TRAIN_DIR, META_DIR
+
+from utils.constants import DATA_PATH, DATASET_FLAG, TRAIN_DIR
 
 
 class FilePath:
     def __init__(self):
         self.meta_files = None
-        self.meta_csv = None
         self.train_dir = None
         self.test_csv = None
 
     def load_all(self):
         if DATASET_FLAG == 0:
-            self.meta_files = glob.glob(os.path.join(DATA_PATH, META_DIR, "*.*"))
-            self.meta_csv = pd.read_csv(os.path.join(DATA_PATH, "Meta.csv"))
             self.train_dir = os.listdir(os.path.join(DATA_PATH, "Train"))
             self.test_csv = pd.read_csv(os.path.join(DATA_PATH, "Test.csv"))
         else:
